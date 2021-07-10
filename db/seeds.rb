ca17ccfd042ee82646c2311431b027c7332ad4e5
Ieva Aleksandravica <ieva.aleksandravica@gmail.com>
2021-07-10 15:47:46 +0200

    model and seed created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  article = Article.new(
+    title: Faker::TvShows::HowIMetYourMother.catch_phrase,
+    content: Faker::TvShows::HowIMetYourMother.quote #=> "Whenever I’m sad, I stop being sad and be awesome instead."
+  )
+  article.save!
+  puts "#{article.title} created!"
+end
